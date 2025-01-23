2e1b8410c7dc22683440a17e45763eb9abc17490
Cemus <24947582+Cemus@users.noreply.github.com>
2025-01-23 10:02:20 +0100

    Fix target in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import * as dotenv from "dotenv";
 
-dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: process.env.VITE_API_URL,
+        target: "http://backend:3000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
