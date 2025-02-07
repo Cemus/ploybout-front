44dc505f3903ed0762ca28ba8c8a332d3f88b052
Cemus <24947582+Cemus@users.noreply.github.com>
2025-02-07 16:28:35 +0100

    Fix proxy error

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": {
-        target: "http://backend:3000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
+      "/api": "http://localhost:3000",
     },
   },
   plugins: [react()],
