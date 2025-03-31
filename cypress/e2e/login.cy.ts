33217316858d94f2e6b0db3040bbe6bb38ebe0d8
Cemus <24947582+Cemus@users.noreply.github.com>
2025-03-31 20:32:27 +0200

    Fix test

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,18 +1,16 @@
-describe("Se crée un compte", () => {
+describe("Se connecter à un compte", () => {
   it("Devrait afficher la page de connexion", () => {
     cy.visit("http://localhost:5173/login");
   });
   it("Devrait pouvoir se connecter", () => {
-    it("Devrait pouvoir s'inscrire", () => {
-      cy.visit("http://localhost:5173/login");
+    cy.visit("http://localhost:5173/login");
 
-      cy.get('input[name="username"]').type("azeazeaze");
-      cy.get('input[name="password"]').type("azeazeaze");
+    cy.get('input[name="username"]').type("azeazeaze");
+    cy.get('input[name="password"]').type("azeazeaze");
 
-      cy.get('button[type="submit"]').click();
+    cy.get('button[type="submit"]').click();
 
-      cy.url().should("include", "/profile");
-      cy.contains("azeazeaze").should("be.visible");
-    });
+    cy.url().should("include", "/profile");
+    cy.contains("azeazeaze").should("be.visible");
   });
 });
