7f558f6d83e52dc86c8523e375aa687b3fd2ac9c
Cemus <24947582+Cemus@users.noreply.github.com>
2025-03-31 20:26:55 +0200

    Connection test

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -2,4 +2,17 @@ describe("Se crée un compte", () => {
   it("Devrait afficher la page de connexion", () => {
     cy.visit("http://localhost:5174/login");
   });
+  it("Devrait pouvoir se connecter", () => {
+    it("Devrait pouvoir s'inscrire", () => {
+      cy.visit("http://localhost:5174/login");
+
+      cy.get('input[name="username"]').type("azeazeaze");
+      cy.get('input[name="password"]').type("azeazeaze");
+
+      cy.get('button[type="submit"]').click();
+
+      cy.url().should("include", "/profile");
+      cy.contains("azeazeaze").should("be.visible");
+    });
+  });
 });
