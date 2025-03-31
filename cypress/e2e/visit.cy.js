775681db79462d1b0bbf36429caa4362e122d73e
Cemus <24947582+Cemus@users.noreply.github.com>
2025-03-31 19:47:33 +0200

    Change address (dev)

--- cypress/e2e/visit.cy.js
+++ cypress/e2e/visit.cy.js
@@ -2,9 +2,9 @@ import { describe, it, cy } from "cypress";
 
 describe("Visite le serveur dev", () => {
   it("Devrait afficher la page d'accueil", () => {
-    cy.visit("http://localhost:5173/");
+    cy.visit("http://localhost:5174/");
   });
   it("Devrait afficher les conditions d'utilisation", () => {
-    cy.visit("http://localhost:5173/gcu");
+    cy.visit("http://localhost:5174/gcu");
   });
 });
