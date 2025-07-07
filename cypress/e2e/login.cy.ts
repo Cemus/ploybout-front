0efe9d03cf31c38a5c5422ebfd93a4ee9cfcb808
Cemus <24947582+Cemus@users.noreply.github.com>
2025-07-07 15:08:29 +0200

    Fix front-back

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -8,11 +8,23 @@ describe("Se connecter à un compte", () => {
   });
 
   it("Devrait pouvoir se connecter", () => {
-    cy.get('input[name="username"]').type("azeazeaze");
-    cy.get('input[name="password"]').type("azeazeaze");
+    cy.log("Remplissage du username");
+    cy.get('input[name="username"]')
+      .type("azeazeaze")
+      .should("have.value", "azeazeaze");
 
-    cy.get('button[type="submit"]').click();
+    cy.log("Remplissage du password");
+    cy.get('input[name="password"]')
+      .type("azeazeaze")
+      .should("have.value", "azeazeaze");
 
+    cy.log("Clic sur le bouton submit");
+    cy.get('button[type="submit"]').should("be.enabled").click();
+
+    cy.log("Vérification de l'URL");
+    cy.url().should("include", "/profile");
+
+    cy.log("Vérification de la présence du nom d'utilisateur");
     cy.contains("azeazeaze").should("be.visible");
   });
 });
